f7677f9f4fe31306fef16f90094fa6585c21326f
Luiz Fernando <luyzfernando@gmail.com>
2024-07-27 17:14:09 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,76 @@
+# Modelo de Skill Alexa para integrar o Home Assistant via Assist Conversation API
+Use o Home Assistant Assist Conversation na Alexa 😊
+
+## Instruções
+
+### Configuração do Home Assistant
+- Crie/Ative uma API Home Assistant para seu usuário e obtenha um token de acesso de longa duração.
+
+### Criação da Skill Alexa
+1. Crie uma Skill Alexa-hosted (Python) na Alexa Developer Console:
+   - **Name your Skill**: Escolha um nome de sua preferência (Ex: HomeAssist)
+   - **Choose a primary locale**: Portuguese (BR)
+   - **Tipo de experiência**: Other > Custom > Alexa-hosted (Python)
+   - **Hosting region**: Pode deixar o padrão (US East (N. Virginia))
+   - **Templates**: Clique em Import Skill
+   - **Insira o endereço**: [https://github.com/luyzfernando08/home-assistant-conversation-alexa-skill.git](https://github.com/luyzfernando08/home-assistant-conversation-alexa-skill.git)
+
+2. Vá na aba "Code"
+3. Insira suas informações no arquivo `config.txt`:
+   - Abra o arquivo `config.txt` no diretório raiz do projeto (/Skill Code/lambda/).
+   - Insira as seguintes informações:
+     ```txt
+     home_assistant_url=https://YOUR-HOME-ASSISTANT-URL:8123/api/conversation/process
+     home_assistant_token=YOUR-HOME-ASSISTANT-TOKEN
+     home_assistant_agent_id=YOUR-AGENT-ID
+     alexa_speak_output=SUA-FRASE
+     alexa_speak_waiting=SUA-FRASE
+     alexa_bye_bye=SUA-FRASE
+     ```
+   - **home_assistant_url**: URL da API de conversação do seu Home Assistant (incluindo a porta padrão 8123).
+   - **home_assistant_token**: Token de acesso de longa duração do seu Home Assistant.
+   - **home_assistant_agent_id**: ID do agente de conversação configurado no seu Home Assistant.
+   - **alexa_speak_output**: Texto que a Alexa irá pronunciar após a ativação da Skill. Você pode utilizar textos randômicos separando as frases por ";"
+   - **alexa_speak_waiting**: Texto que a Alexa irá pronunciar caso não seja falado nenhum comando.
+   - **alexa_bye_bye**: Texto de tchau da Alexa. Você pode utilizar textos randômicos separando as frases por ";"
+
+4. Salve as alterações.
+
+### Configurando o Invocation Name
+- O nome de invocação padrão configurado no código é **ATENÇÃO**.
+- Para alterar o nome de invocação:
+  1. Vá para a aba "Build" no Alexa Developer Console.
+  2. Clique em "Invocations" e depois em "Skill Invocation Name".
+  3. Insira o novo nome de invocação desejado e salve as alterações.
+  4. Rebuild o modelo (Build skill).
+
+### Obtendo o `home_assistant_agent_id`
+- O `agent_id` pode ser encontrado no assistente de debug para o seu agente de conversação alvo: 
+  1. Acesse **Settings** > **Voice assistants** > **OpenAI** (ou o nome que você deu ao assistente OpenAI) > menu de três pontos > **Debug**.
+  2. O `agent_id` será exibido na seção de debug.
+  - Veja a imagem a seguir para referência:
+    ![Debug Assistant](![alt text](image.png))
+
+### Deploy da Skill
+1. Faça Build do Modelo e Deploy do Código na aba "Deploy".
+2. Teste a Skill no console da Alexa Developer para garantir que está funcionando corretamente.
+
+### Configuração Extra
+  - Se a Alexa não receber nenhum comando, ela irá perguntar o texto que você configurou "alexa_speak_waiting".
+    Adicione uma automação no Home Assistant com a palavra "Sim" ou qualquer outra coisa para que a Alexa possa escutar novamente seus comandos.
+
+  ```yaml
+    alias: Sim
+    description: ""
+    trigger:
+      - platform: conversation
+        command: sim
+    condition: []
+    action:
+      - set_conversation_response: Pode falar
+    mode: single
+  ```
+
+### Boa sorte!
+Agora você pode usar sua Skill Alexa para integrar e interagir com o Home Assistant via Assist Conversation API.
+Se gostou, lembre-se de mandar um ""Obrigado"" para o desenvolvedor.
---
+++ lambda/lambda_function.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+import json
+import requests
+import ask_sdk_core.utils as ask_utils
+import random
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Função para carregar configurações do arquivo
+def load_config():
+    config = {}
+    try:
+        with open("config.txt", encoding='utf-8') as f:
+            for line in f:
+                name, value = line.strip().split('=')
+                config[name] = value
+    except Exception as e:
+        logger.error(f"Erro ao carregar o arquivo de configuração: {str(e)}")
+    return config
+
+config = load_config()
+
+# Configurações do Home Assistant
+home_assistant_url = config.get("home_assistant_url")
+home_assistant_token = config.get("home_assistant_token")
+home_assistant_agent_id = config.get("home_assistant_agent_id")
+alexa_speak_output = config.get("alexa_speak_output").strip().split(';')
+alexa_speak_waiting = config.get("alexa_speak_waiting")
+alexa_bye_bye = config.get("alexa_bye_bye").strip().split(';')
+
+# Verificação de configuração
+if not alexa_bye_bye or not home_assistant_url or not home_assistant_token or not home_assistant_agent_id or not alexa_speak_output or not alexa_speak_waiting:
+    raise ValueError("alexa_bye_bye, alexa_speak_waiting, alexa_speak_output, home_assistant_url, home_assistant_token ou home_assistant_agent_id não configurados corretamente")
+
+# Variável global para armazenar o conversation_id
+conversation_id = None
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        global conversation_id
+        conversation_id = None  # Reseta o conversation_id para uma nova sessão
+        msg = random.choice(alexa_speak_output)
+        return handler_input.response_builder.speak(msg).ask(msg).response
+
+class HomeAssistantQueryIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_intent_name("HomeAssistantQueryIntent")(handler_input)
+
+    def handle(self, handler_input):
+        query = handler_input.request_envelope.request.intent.slots["query"].value
+        logger.info(f"Query received: {query}")
+        response = process_conversation(query)
+        logger.info(f"Response generated: {response}")
+        return handler_input.response_builder.speak(response).ask(alexa_speak_waiting).response
+
+def process_conversation(query):
+    global conversation_id
+    try:
+        headers = {
+            "Authorization": f"Bearer {home_assistant_token}",
+            "Content-Type": "application/json",
+        }
+        data = {
+            "text": replace_words(query),
+            "language": "pt-BR",
+            "agent_id": home_assistant_agent_id
+        }
+        if conversation_id:
+            data["conversation_id"] = conversation_id
+
+        logger.debug(f"Requesting Home Assistant with data: {data}")
+        response = requests.post(home_assistant_url, headers=headers, json=data)
+        logger.debug(f"Home Assistant response status: {response.status_code}")
+        logger.debug(f"Home Assistant response data: {response.text}")
+        
+        response_data = response.json()
+
+        if response.status_code == 200 and "response" in response_data:
+            conversation_id = response_data.get("conversation_id", conversation_id)
+            response_type = response_data["response"]["response_type"]
+            if response_type == "action_done" or response_type == "query_answer":
+                speech = response_data["response"]["speech"]["plain"]["speech"]
+            elif response_type == "error":
+                speech = response_data["response"]["speech"]["plain"]["speech"]
+                logger.error(f"Error code: {response_data['response']['data']['code']}")
+            else:
+                speech = "Não consegui processar sua solicitação."
+            return speech
+        else:
+            error_message = response_data.get("message", "Erro desconhecido")
+            logger.error(f"Erro ao processar a solicitação: {error_message}")
+            return "Desculpe, não consegui entender seu pedido."
+
+    except Exception as e:
+        logger.error(f"Erro ao gerar resposta: {str(e)}", exc_info=True)
+        return f"Erro ao gerar resposta: {str(e)}"
+
+def replace_words(query):
+    query = query.replace('4.º','quarto')
+    return query
+
+class HelpIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speak_output = "Como posso te ajudar?"
+        return handler_input.response_builder.speak(speak_output).ask(speak_output).response
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speak_output = random.choice(alexa_bye_bye)
+        return handler_input.response_builder.speak(speak_output).response
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        return handler_input.response_builder.response
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        logger.error(exception, exc_info=True)
+        speak_output = f"Desculpe, não consegui processar sua solicitação. {str(e)}"
+        return handler_input.response_builder.speak(speak_output).ask(speak_output).response
+
+sb = SkillBuilder()
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HomeAssistantQueryIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()
+
